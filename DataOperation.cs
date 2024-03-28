4fd2af5257ebf72b4e5ce9c29d5e7e215ecc272f
Your Name <shadisaad911@gmail.com>
2024-03-28 16:03:56 +0300

    Add project files.

---
+++ DataOperation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MznQuestion
+{
+	public class DataOperation
+	{
+		private readonly ProductsDbContext context;
+
+		public DataOperation(ProductsDbContext context)
+		{
+			this.context = context;
+		}
+
+		public void ExportDataToJson(string FilePath)
+		{
+			var Products = context.ProductInformations.ToList();
+			string json = JsonSerializer.Serialize(Products);
+			File.WriteAllText(FilePath, json);
+			Console.WriteLine("The File was Exported successfully");
+		}
+
+		public void ImportDataFromJson(string FilePath)
+		{
+			string json = File.ReadAllText(FilePath);
+			List<ProductInformation> products = JsonSerializer.Deserialize<List<ProductInformation>>(json);
+
+				foreach (var product in products)
+				{
+					context.ProductInformations.Add(product);
+				}
+
+				try
+				{
+					context.SaveChanges();
+				 Console.WriteLine("The File was imported successfully");
+				 }
+				catch (Exception ex)
+				{
+					 Console.WriteLine(" Error Type: " +ex.GetType());
+				}
+		}
+
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MznQuestion
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var context = new ProductsDbContext();
+			var DataOperation=new DataOperation(context);
+			string input;
+			while(true)
+			{
+
+                Console.WriteLine("1-import json file to database");
+                Console.WriteLine("2-Export json file From database");
+                Console.WriteLine("3-Stop App");
+                Console.WriteLine("4-Clear History");
+                int x = int.Parse(Console.ReadLine());
+				switch(x)
+				{
+					case 1: Console.WriteLine("Write The File Path: Example: D:\\\\FileName.json "); input = Console.ReadLine(); DataOperation.ImportDataFromJson(input); break;
+					case 2: Console.WriteLine("Write The File Path: Example: D:\\\\FileName.json "); input = Console.ReadLine(); DataOperation.ExportDataToJson(input); break;
+					case 3:break;
+					case 4:Console.Clear();break;
+				}
+				if (x == 3)
+				{
+					break;
+				}
+				
+			}  
+			
+		}
+	}
+	
+}
